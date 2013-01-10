aaef84f636b496c47d3affac0e8995eeedcf156e
joelevin <joe.levin@gmail.com>
2013-01-09 19:08:57 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Postboard::Application.config.secret_token = '53956191949c9edc8a13aa031c92ac30add3e3570c2eee77eac54d286e1a4c567c1c64845b1a7cbcdf67f8dce444d4663058b66add95ae30ab98e08b0a5ff291'
